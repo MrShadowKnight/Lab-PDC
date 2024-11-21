dcf1153293f0b2d2cc3a0dc99caf6af9dd0240ac
Mr_ShadowKnight_ <volodymyr.pinchak@gmail.com>
2024-11-21 19:05:22 +0200

    Add.main

---
+++ Lab-4.2/Lab-4.2/Lab-4.2.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<vector<int>> lexicographicCombinations(int n, int r) {
+    vector<vector<int>> result;
+    vector<int> combination(r);
+
+    for (int i = 0; i < r; ++i) {
+        combination[i] = i + 1;
+    }
+    result.push_back(combination);
+
+    while (true) {
+        int i = r - 1;
+        while (i >= 0 && combination[i] == n - r + i + 1) {
+            --i;
+        }
+
+        if (i < 0) {
+            break;
+        }
+
+        ++combination[i];
+        for (int j = i + 1; j < r; ++j) {
+            combination[j] = combination[j - 1] + 1;
+        }
+        result.push_back(combination);
+    }
+
+    return result;
+}
+
+int main() {
+    int n, r;
+    cout << "Enter n = ";
+    cin >> n;
+    cout << "Enter r = ";
+    cin >> r;
+
+    vector<vector<int>> combinations = lexicographicCombinations(n, r);
+    for (const auto& combination : combinations) {
+        cout << "combination = {";
+        for (size_t i = 0; i < combination.size(); ++i) {
+            cout << combination[i] << (i < combination.size() - 1 ? ", " : "");
+        }
+        cout << "}" << endl;
+    }
+    return 0;
+}
---
+++ Lab-4.2/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab-4.2/Lab-4.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+namespace UnitTest
+{
+    TEST_CLASS(UnitTest)
+    {
+    public:
+
+        TEST_METHOD(TestCombinations)
+        {
+            // Вхідні параметри
+            int n = 5;
+            int r = 3;
+
+            // Очікуваний результат
+            vector<vector<int>> expected = {
+                {1, 2, 3}, {1, 2, 4}, {1, 2, 5},
+                {1, 3, 4}, {1, 3, 5}, {1, 4, 5},
+                {2, 3, 4}, {2, 3, 5}, {2, 4, 5},
+                {3, 4, 5}
+            };
+
+            // Виклик функції
+            vector<vector<int>> result = lexicographicCombinations(n, r);
+
+            // Перевірка розміру
+            Assert::AreEqual(expected.size(), result.size(), L"Розмір множини неправильний");
+
+            // Перевірка кожної комбінації
+            for (size_t i = 0; i < expected.size(); ++i) {
+                Assert::IsTrue(expected[i] == result[i], L"Комбінація не співпадає");
+            }
+        }
+    };
+}
